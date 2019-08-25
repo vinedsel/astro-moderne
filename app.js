33e9d7fab172e0fb4f5085ad64eb40975eeda848
Raymond Gregory <vinedsel@gmail.com>
2019-08-25 03:56:47 -0500

    Unordered list structure scrapped for the presentation modules. Layout is mostly done. Still fixing search feature. And added a frame to the carousel

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
     autoplaySpeed: 3000
   });
 });
+
+// Search Engine
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("stack");
+  li = ul.getElementsByTagName("h4");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("article")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
